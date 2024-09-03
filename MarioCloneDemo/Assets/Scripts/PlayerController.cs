544a3b8a2609bde7ca0b27a3b0c75e5e7a8e44f2
maklsmit <156136111+maklsmit@users.noreply.github.com>
2024-09-03 17:44:19 -0400

    Practice1

---
+++ MarioCloneDemo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move(){
+        float moveInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * moveInput * runSpeed * Time.deltaTime);
+    }
+
+    private void Jump(){
+        if(Input.GetButtonDown("Jump")){
+            rb2d.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+}
